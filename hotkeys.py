e7c87aad9a19a2a586496f77511d1d07dab6b7f6
Spyboy <spyboy.spice@gmail.com>
2020-11-27 07:12:54 +0530

    Create hotkeys.py

---
+++ hotkeys.py
@@ -0,0 +1,18 @@
+import pyautogui
+from time import sleep
+
+pyautogui.hotkey("command", "space")
+pyautogui.typewrite("Sublime Text.app")
+pyautogui.typewrite(["enter"])
+sleep(1.5)
+pyautogui.typewrite("hi i am shubham, founder & developer of spyboy app.")
+pyautogui.hotkey("command", "a")
+pyautogui.hotkey("command", "c")
+pyautogui.typewrite(["enter"])
+pyautogui.hotkey("command", "v")
+pyautogui.typewrite(["enter"])
+pyautogui.hotkey("command", "v")
+pyautogui.typewrite(["enter"])
+pyautogui.hotkey("command", "v")
+pyautogui.typewrite(["enter"])
+pyautogui.hotkey("command", "v")
