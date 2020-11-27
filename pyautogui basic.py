2ddf738128f8d6319196a98f09963d1b5a1a397e
Spyboy <spyboy.spice@gmail.com>
2020-11-27 06:54:27 +0530

    Update pyautogui basic.py

--- pyautogui basic.py
+++ pyautogui basic.py
@@ -1,16 +1,34 @@
+
+"""
+                  _                 
+                 | |                
+  ___ ____  _   _| |__   ___  _   _ 
+ /___)  _ \| | | |  _ \ / _ \| | | |
+|___ | |_| | |_| | |_) ) |_| | |_| |
+(___/|  __/ \__  |____/ \___/ \__  |
+     |_|   (____/            (____/   
+"""
+# -----------------------------------------------------------
+# Python3 or above must be installed on your System.
+# Install pyautogui
+# To install type "pip install pyautogui"
+# pip install pyobjc (only for mac)
+# pip install pyobjc-core (only for mac)
+# ChromeDriverManager must be installed on the system
+# To install type "pip install webdriver_manager"
+# R0cK & R0oL !!
+# -----------------------------------------------------------
+
 import pyautogui
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-#Point(x=998, y=60) >ctrl + shift + r
+
+#Point(x=998, y=60) >search bar postion >ctrl + shift + r
 #ctrl + s > save
 #ctrl + shift + r  >run
 
-print(pyautogui.position())
-pyautogui.click(998, 60)
-#pyautogui.typewrite("spyboy blog")
-#pyautogui.typewrite(["enter"])
-pyautogui.hotkey("command", "c")
-pyautogui.hotkey("command", "space")
-sleep(3.5)
-pyautogui.hotkey("command", "v")
+print(pyautogui.position()) 
+pyautogui.click(998, 60)                   #search bar postion
+pyautogui.typewrite("spyboy.blog")
+pyautogui.typewrite(["enter"])
