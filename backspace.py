b6a38e994359c64a2a16def112f28f1075d21fae
Spyboy <spyboy.spice@gmail.com>
2020-12-05 04:40:46 +0530

    Update backspace.py

--- backspace.py
+++ backspace.py
@@ -1,8 +1,27 @@
+  
+
+"""
+                  _                 
+                 | |                
+  ___ ____  _   _| |__   ___  _   _ 
+ /___)  _ \| | | |  _ \ / _ \| | | |
+|___ | |_| | |_| | |_) ) |_| | |_| |
+(___/|  __/ \__  |____/ \___/ \__  |
+     |_|   (____/            (____/   
+"""
+# -----------------------------------------------------------
+# Python3 or above must be installed on your System.
+# Install pyautogui
+# To install type "pip install pyautogui"
+# pip install pyobjc (only for mac)
+# pip install pyobjc-core (only for mac)
+# R0cK & R0oL !!
+# -----------------------------------------------------------
 import pyautogui
 from time import sleep
 
-pyautogui.hotkey("command", "space")
-pyautogui.typewrite("Sublime Text.app")
+pyautogui.hotkey("command", "space") #change hotkey() accordingly 
+pyautogui.typewrite("Sublime Text.app") #change typewrite() accordingly 
 pyautogui.typewrite(["enter"])
 sleep(1.5)
 pyautogui.hotkey("down", "right")
