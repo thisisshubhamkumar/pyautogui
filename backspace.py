bb9e297bc702e16f3e22fdcd63d701f041bce850
Spyboy <spyboy.spice@gmail.com>
2020-12-05 05:20:52 +0530

    Update backspace.py

--- backspace.py
+++ backspace.py
@@ -27,7 +27,15 @@ sleep(1.5)  #change sleep time accordingly
 pyautogui.hotkey("down", "right")
 pyautogui.hotkey("backspace")
 x = 1
-while x < 161: # #change loop number accordingly 
+while x < 300: # #change loop number accordingly 
     pyautogui.hotkey("down", "right")
     pyautogui.hotkey("backspace")
     x += 1
+# -----------------------------------------------------------
+# there r times when u need to press some key hundreds of time for some specific task like >> need to press (delete/backspace) key in my code to remove some error.
+# i write this code to fix (IndentationError:) in python but it can be used anywhere.
+# because of the error i had to press (down arrow key, right arrow key & backsapce key) around 300 times & that’s exhausting.
+# so i made this bot >> programmed it to run specific keystroke on loop.
+# so whn u run the code it opens >> "serach menu"  & then type>> "Sublime Text editor"  & then hit >> enyter & then>> "there’s sleep time of 2 sec" & then >> it press "down arrow key, right arrow key, backsapce.
+# & it's repeat same thing 300 times.
+# -----------------------------------------------------------
